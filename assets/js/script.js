aff66b83d7bc10377aed261e7395281f11eebe7e
Jonathan Hanahan <jonathan.hanahan@gmail.com>
2024-01-23 17:44:16 -0600

    initial bootstrap layout and variables

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+var scores = document.querySelector("#score");
+var timer = document.querySelector("#timer");
+var question = document.querySelector("#question");
+var answer1 = document.querySelector("#a1");
+var answer2 = document.querySelector("#a2");
+var answer3 = document.querySelector("#a3");
