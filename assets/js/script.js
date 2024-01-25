f3a7c77d23d6ceb68df8ca2c1a5c26f206a27a02
Jonathan Hanahan <jonathan.hanahan@gmail.com>
2024-01-25 12:53:46 -0600

    On click event that moves through questions when an answer button is clicked.

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,3 +4,37 @@ var question = document.querySelector("#question");
 var answer1 = document.querySelector("#a1");
 var answer2 = document.querySelector("#a2");
 var answer3 = document.querySelector("#a3");
+
+// Used to move from Start through questions to End
+var q=0;
+console.log(q);
+
+
+function nextQuestion(){
+
+    if (q==0){
+question.textContent = "Do you want to take a quiz?";
+answer1.textContent = "yes";
+answer2.textContent = "no";
+
+q++;
+console.log(q);
+
+    } else if (q == 1){
+
+question.textContent = "Question 1?";
+answer1.textContent = "Answer 1";
+answer2.textContent = "Answer 2";
+
+q++;
+
+} else if (q == 2){
+
+question.textContent = "Question 2?";
+answer1.textContent = "Answer 2-1";
+answer2.textContent = "Answer 2-2";
+q++;
+
+}
+}
+
