0f66dbfd07d19ad554b0cc75f2e521b09e1e16c1
Jonathan Hanahan <jonathan.hanahan@gmail.com>
2024-01-29 15:18:35 -0600

    updated placeholders to real questions and toggled correct class

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,35 +6,57 @@ var answer2 = document.querySelector("#a2");
 var answer3 = document.querySelector("#a3");
 
 // Used to move from Start through questions to End
+// Question Count
 var q=0;
 console.log(q);
+
+// Correct Answer Score 
+var c=0;
 
 
 function nextQuestion(){
 
     if (q==0){
-question.textContent = "Do you want to take a quiz?";
-answer1.textContent = "yes";
-answer2.textContent = "no";
+   question.textContent = "Commonly Used Datatypes do NOT include: ";
+answer1.textContent = "Strings";
+answer2.textContent = "Alerts";
+    correct();
+// Add correct class
 
-q++;
-console.log(q);
+// Remove incorerct class
+
+    q++;
+    console.log(q);
 
     } else if (q == 1){
-
-question.textContent = "Question 1?";
-answer1.textContent = "Answer 1";
-answer2.textContent = "Answer 2";
-
+question.textContent = "Conditions in an if/else statement are enclosed in _______?";
+answer1.textContent = "Brackets";
+answer2.textContent = "Parenthesis";
+ correct();
 q++;
+console.log(q);
+answer1.classList.remove("correct");
+answer2.classList.add("correct");
+
 
 } else if (q == 2){
 
-question.textContent = "Question 2?";
-answer1.textContent = "Answer 2-1";
-answer2.textContent = "Answer 2-2";
+question.textContent = "A useful development tool for printing content to the console is?";
+answer1.textContent = "console.log();";
+answer2.textContent = "web inspector";
+ correct();
 q++;
+console.log(q);
+answer1.classList.add("correct");
+answer2.classList.remove("correct");
 
+
+} else {
+ question.textContent = "Results";
+    answer1.textContent = "Exit?";
+    answer2.textContent = "Play Again?";
+    
+   
 }
 }
 
@@ -44,4 +66,18 @@ function exit(){
     question.textContent = "Ok bye";
     answer1.remove();
     answer2.remove();
+}
+
+
+function correct() {
+    if (event.target.classList.contains('correct')) {
+    console.log('Element with class "correct" was clicked!');
+    c=c+10;
+    console.log(c);
+
+  } else{
+    console.log('Element with the class "wrong" was clicked!');
+     c=c-10;
+    console.log(c);
+  } 
 }
